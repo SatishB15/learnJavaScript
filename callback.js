861d6192b8ecc38611151ad196282d89d9a2f136
SatishB15 <satishbirhade16@gmail.com>
2021-11-28 19:58:18 +0530

    New Files Added

--- callback.js
+++ callback.js
@@ -15,35 +15,35 @@ which is then invoked inside the outer function to complete some kind of routine
 // message();
 
 // function
-function greet(name, callback) {
-    console.log('Hi' + ' ' + name);
-    callback();
-}
+// function greet(name, callback) {
+//     console.log('Hi' + ' ' + name);
+//     callback();
+// }
 
 // callback function
-function callMe() {
-    console.log('How are You....!');
-}
+// function callMe() {
+//     console.log('How are You....!');
+// }
 
 // passing function as an argument
-greet('Peter', callMe);
+// greet('Peter', callMe);
 
-// const movies = [
-//     { title:"Kabir singh",year:"2019"},
-//     { title:"Sooryavanshi",year:"2021"}];
+const movies = [
+    { title:"Kabir singh",year:"2019"},
+    { title:"Sooryavanshi",year:"2021"}];
 
-// function getMovies(){
-// setTimeout(() => {
-//     movies.forEach((movie, index) => {
-//         console.log(movie.title)
-//     })
-// }, 2000);
-// }
+function getMovies(){
+setTimeout(() => {
+    movies.forEach((movie, index) => {
+        console.log(movie.title)
+    })
+}, 2000);
+}
 
-// function createMovies(movie,callback){
-// setTimeout(() => {
-//     movies.push(movie);
-//     callback();
-// }, 4000);
-// }
-// createMovies({ title:"Jay Bhim", year:"2021"},getMovies);
+function createMovies(movie,callback){
+setTimeout(() => {
+    movies.push(movie);
+    callback();
+}, 4000);
+}
+createMovies({ title:"Jay Bhim", year:"2021"},getMovies);
